77062ed748abd48defa364f591f43f6e2d6ea315
gonghongcai <1046244169@qq.com>
2017-06-03 22:45:50 +0800

    hello world

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * <p> </p>
+ *
+ * @Author : gonghongcai.
+ * @Date : 2017/6/3 22:44 .
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello gitHub");
+    }
+}
